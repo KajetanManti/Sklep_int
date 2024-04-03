51d50770c445fc1a2dcacf09da4ad041d3655ba4
KajetanManti <kajetan.manturewicz@gmail.com>
2024-04-03 13:16:54 +0200

    first commit

---
+++ Sklep.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="widht=device-widht">
+<link rel="stylesheet" href=Sklep.css>
+<title>SKLEP</title>
+</head>
+<body>
+    <header class="flex-container topnav">
+        <img src="img/logo.jpg" alt="logo" id="logo" >
+        <form action="Sklep.php" method="get">
+        <input type="text" name="search" id="search" placeholder="Wyszukaj produkt">
+        <input type="submit" value="Szukaj">
+        </form>
+
+        <a href="Koszyk.php" ><img src="img/koszyk.png" alt="koszyk" id="Koszyk" ></a>
+    </header>
+
+    <main class='flex-container-wrap'>
+        <div class='card'>
+            <h3>Nazwa oferty</h3>
+            <p>Cena:100zł<p>
+        </div>
+        <div class='card'>
+            
+        </div>
+        <div class='card'>
+            
+        </div>
+        <div class='card'>
+            
+        </div>
+        <div class='card'>
+            
+        </div>
+        <div class='card'>
+            
+        </div>
+    </main>
+ 
+</body>
+</html>
